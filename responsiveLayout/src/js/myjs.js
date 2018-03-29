e5e4d0f2924f5a79cdc48cf906d6665af3fe5ac6
Cauzinc <514322058@qq.com>
2018-03-29 11:24:58 +0800

    响应式布局

--- responsiveLayout/src/js/myjs.js
+++ responsiveLayout/src/js/myjs.js
@@ -1,12 +1,24 @@
 $(function(){
+
     $("#banner").owlCarousel({
         items:1,
        autoplay:true,
-       autoplayTimeout:1000,
+       autoplayTimeout:2000,
         loop:true,
         autoplayHoverPause:true
     });
 
+    $("#tabs li").mouseover(function(){
+        var index=0;
+        $(this).addClass("active").siblings().removeClass();
+        index = $(this).index();
+        $("#columnsTab #content div").eq(index).addClass("conShow").siblings().removeClass("conShow");
+    });
+
+    $("#columnsTab #content ul li").mouseover(function(){
+        // $(this).stop(true).animate({width:800},500).siblings().stop(true).animate({width:160},500);
+
+    });
 
 
 
